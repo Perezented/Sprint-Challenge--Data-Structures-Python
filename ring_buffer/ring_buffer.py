7196255c9fc3407582daae3878b1b2cb286362cd
Michael Perez <mperez6473@gmail.com>
2020-07-17 10:29:43 -0500

    Its sending the appended items over but they are in backwards.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,7 +4,20 @@ class RingBuffer:
         self.items = []
 
     def append(self, item):
-        self.items.insert(self.capacity, item)
+        # if the length of the items is less than or equal to the self.capacity,
+        # let the items in
+        counter = 0
+        if len(self.items) < self.capacity:
+            self.items.insert(self.capacity, item)
+        if len(self.items) == self.capacity:
+            self.items.insert(counter, item)
+            self.items.pop()
+            counter += 1
+            if counter == self.capacity:
+                counter = 0
 
+        # else if the lenght is the self.capacity, have it insert to 0 up to the self.capacity
+
+    # return all items in the ring buffer list
     def get(self):
         return self.items
